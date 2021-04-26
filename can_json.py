861fcba136bf8d09aeed461c30a8df16a3855400
TimHasert <timhasert@gmail.com>
2021-04-26 10:44:06 +0200

    refactored message attribute list into const, check for it during parse

--- can_json.py
+++ can_json.py
@@ -5,6 +5,20 @@ can_json.py
 import json
 from can import Message
 
+MESSAGE_ATTRIBUTES = {
+    "arbitration_id",
+    "bitrate_switch",
+    "channel",
+    "data",
+    "dlc",
+    "error_state_indicator",
+    "is_error_frame",
+    "is_extended_id",
+    "is_fd",
+    "is_remote_frame",
+    "timestamp"
+}
+
 
 def json_parse(self: Message, json_string: str) -> None:
     """Decodes a json-encoded can.Message and passes its attribute values to object instance"""
@@ -12,33 +26,30 @@ def json_parse(self: Message, json_string: str) -> None:
     this_dict: dict = json.loads(json_string)
     # iterate over all dict items
     for item in this_dict.items():
-        # normal case
-        if item[0] != "data":
-            setattr(self, item[0], item[1])
         # special case 'data', requires typecast
-        else:
+        if item[0] == "data":
             this_data = bytearray()
             for ele in item[1]:
                 this_data.append(ele)
             setattr(self, 'data', this_data)
+        # normal case
+        elif item[0] in MESSAGE_ATTRIBUTES:
+            setattr(self, item[0], item[1])
+        # json attribute not part of can.Message
+        else:
+            pass
 
 
 def json_stringify(self: Message) -> str:
     """Encodes can.Message as json string and returns it"""
     # init and populate dict
-    this_dict = {}
-    this_dict["arbitration_id"] = self.arbitration_id
-    this_dict["bitrate_switch"] = self.bitrate_switch
-    this_dict["channel"] = self.channel
-    this_dict["dlc"] = self.dlc
-    this_dict["error_state_indicator"] = self.error_state_indicator
-    this_dict["is_error_frame"] = self.is_error_frame
-    this_dict["is_extended_id"] = self.is_extended_id
-    this_dict["is_fd"] = self.is_fd
-    this_dict["is_remote_frame"] = self.is_remote_frame
-    this_dict["timestamp"] = self.timestamp
-    this_dict["data"] = []
-    for ele in self.data:
-        this_dict["data"].append(ele)
+    out : dict = {}
+    for attr in MESSAGE_ATTRIBUTES:
+        # special case data
+        if attr == 'data':
+            out['data'] = []
+        # normal case
+        else:
+            out[attr] = self.__getattribute__(attr)
     # return exported json
-    return json.dumps(this_dict)
+    return json.dumps(out)
