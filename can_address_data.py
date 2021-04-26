43e51f69761d5adc86cfcfa9939186d4a606360a
TimHasert <timhasert@gmail.com>
2021-04-26 09:33:08 +0200

    moved object map to own function

--- can_address_data.py
+++ can_address_data.py
@@ -1,44 +1,46 @@
 """
-pycan_addons.can_address_data
-
-Adds method to calculate CAN ID and object type of message based on 
-CANopen standard to can.Message class
+can_address_data.py
 """
 
-import can
+from can import Message
 
-def get_address_data(self: can.Message):
-    """Returns node ID and object type of message"""
-    address_data = {}
-    # get node ID
-    address_data["node_id"] = self.arbitration_id & 0b000001111111
-    # get object type, map taken from https://en.wikipedia.org/wiki/CANopen#Predefined_Connection_Set[7]
-    object_map = {
-        0x000                           : "NMT_CONTROL",
-        0x001                           : "FAILSAFE",
-        0x080                           : "SYNC",
-        0x080 + address_data["node_id"] : "EMERGENCY",
-        0x100                           : "TIMESTAMP",
-        0x180 + address_data["node_id"] : "TPDO1",
-        0x200 + address_data["node_id"] : "RPDO1",
-        0x280 + address_data["node_id"] : "TPDO2",
-        0x300 + address_data["node_id"] : "RPDO2",
-        0x380 + address_data["node_id"] : "TPDO3",
-        0x400 + address_data["node_id"] : "RPDO3",
-        0x480 + address_data["node_id"] : "TPDO4",
-        0x500 + address_data["node_id"] : "RPDO4",
-        0x580 + address_data["node_id"] : "TSDO",
-        0x600 + address_data["node_id"] : "RSDO",
-        0x700 + address_data["node_id"] : "NMT_MONITORING",
-        0x7E4                           : "TLSS",
-        0x7E5                           : "RLSS"
+
+def get_object_map(id_offset: int) -> dict:
+    """
+    Returns ID to communication object mapping of a given CAN node ID.
+    Map taken from https://en.wikipedia.org/wiki/CANopen#Predefined_Connection_Set[7]
+    """
+    return {
+        0x000               : "NMT_CONTROL",
+        0x001               : "FAILSAFE",
+        0x080               : "SYNC",
+        0x080 + id_offset   : "EMERGENCY",
+        0x100               : "TIMESTAMP",
+        0x180 + id_offset   : "TPDO1",
+        0x200 + id_offset   : "RPDO1",
+        0x280 + id_offset   : "TPDO2",
+        0x300 + id_offset   : "RPDO2",
+        0x380 + id_offset   : "TPDO3",
+        0x400 + id_offset   : "RPDO3",
+        0x480 + id_offset   : "TPDO4",
+        0x500 + id_offset   : "RPDO4",
+        0x580 + id_offset   : "TSDO",
+        0x600 + id_offset   : "RSDO",
+        0x700 + id_offset   : "NMT_MONITORING",
+        0x7E4               : "TLSS",
+        0x7E5               : "RLSS"
     }
-    address_data["object_type"] = object_map.get(self.arbitration_id, None)
+
+
+def get_address_data(self: Message) -> dict:
+    """Returns node ID and object type of a given message"""
+    address_data : dict = {}
+    # get node ID
+    address_data["node_id"] = (self.arbitration_id & 0b000001111111)
+    # get object type, 
+    address_data["object_type"] = get_object_map(address_data['node_id']).get(self.arbitration_id, None)
     # retroactively overwrite node id for broadcasted objects
     if address_data["object_type"] in ["NMT_CONTROL", "FAILSAFE", "SYNC", "TIMESTAMP", "TLSS", "RLSS"]:
         address_data["node_id"] = -1
     # return final object
     return address_data
-
-# add method to can.Message class
-setattr(can.Message, 'get_address_data', get_address_data)
