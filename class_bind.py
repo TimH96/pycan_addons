8544813caf22e705f46571890da7ddc053df92db
TimHasert <timhasert@gmail.com>
2021-04-26 11:43:25 +0200

    explicit import

--- class_bind.py
+++ class_bind.py
@@ -2,9 +2,9 @@
 class_bind.py
 """
 
-from can                import Message
-from can_json           import *
-from can_address_data   import *
+from can                            import Message
+from pycan_addons.can_json          import *
+from pycan_addons.can_address_data  import *
 
 # add methods to can.Message class
 setattr(Message,    'get_address_data',    get_address_data)
