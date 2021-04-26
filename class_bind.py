796c19fc6153cec1fd9dd23c9bf7d02f1a8c36a6
TimHasert <timhasert@gmail.com>
2021-04-26 09:32:20 +0200

    moved bindings to Message class to own file

---
+++ class_bind.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+from can                import Message
+from can_json           import *
+from can_address_data   import *
+
+# add methods to can.Message class
+setattr(Message,    'get_address_data',    get_address_data)
+setattr(Message,    'json_parse',          json_parse)
+setattr(Message,    'json_stringify',      json_stringify)
